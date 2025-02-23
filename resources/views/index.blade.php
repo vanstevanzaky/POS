fa5832ce46175861a17c6b7a5d19e8f879e1db9f
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-02-23 10:28:38 +0700

    router, controller , dan views halaman utama

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <title>Point of Sales
+    </title>
+</head>
+
+<body>
+    <H1>P O S</H1>  
+    <h4>POINT OF SALES</H4>
+    <P>INI ADALAH HALAMAN UTAMA SEBUAH WEBSITE UNTUK POINT OF SALES</P>
+    <P style="max-width: 80vh;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum arcu massa, at porta massa viverra id.
+        Phasellus nec felis id neque pellentesque pellentesque. In faucibus dictum efficitur. Maecenas aliquam quis
+        augue mollis hendrerit. Vestibulum iaculis sit amet erat ultrices bibendum. Quisque nec tincidunt lorem. Donec
+        cursus massa vel malesuada suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        pellentesque feugiat facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+        turpis egestas. Integer bibendum magna ex, eget accumsan orci eleifend a. Aliquam vulputate arcu sed sapien
+        suscipit, at volutpat dolor pharetra. Vestibulum sed orci sed turpis tincidunt scelerisque id sed diam. Donec
+        lacinia sit amet orci et auctor.
+    </P>
+    
+</body>
+
+</html>
