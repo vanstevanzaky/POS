d83af50292264c5848ec8bd7bbaf237452e66d70
STEVAN ZAKY S <Stevanzaky87@gmail.com>
2025-02-25 20:17:46 +0700

    halaman penjualan

---
+++ resources/views/penjualan.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Halaman Penjualan</title>
+</head>
+<body>
+    <h1>Halaman Penjualan</h1>
+    <p>Menampilkan halaman transaksi POS.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\BeautyHealthController;
 use App\Http\Controllers\FoodBeverageController;
 use App\Http\Controllers\HomeCareController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -29,3 +30,5 @@ Route::prefix('category')->group(function (){
 });
 
 Route::get('/user/{id}/name/{name}/', [UserController::class, 'pengguna']);
+
+Route::get('/penjualan', [PenjualanController::class, 'penjualan']);
